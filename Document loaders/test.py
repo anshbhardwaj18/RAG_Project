824d752af68c38f4a2a15f0cc6eaf3e8ec9b3c81
Ansh Bhardwaj <anshb457@gmail.com>
2026-07-10 17:09:31 +0530

    Updated commit

--- Document loaders/test.py
+++ Document loaders/test.py
@@ -1,7 +1,14 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
 
-data = TextLoader("Document loaders/notes.txt")
+splitter = CharacterTextSplitter(
+    chunk_size = 10,
+    chunk_overlap = 1
+)
+
+data = TextLoader("Document loaders/notes2.txt")
 
 docs = data.load()
+chunks = splitter.split_documents(docs)
 
-print(docs[0])
+print(chunks)
