02c4b7bd5fcb58f082302ca1390315c375f88ab8
Ansh Bhardwaj <anshb457@gmail.com>
2026-07-10 23:31:31 +0530

    Updated Commit

--- Document loaders/pdf.py
+++ Document loaders/pdf.py
@@ -1,16 +1,15 @@
 from langchain_classic.document_loaders import PyPDFLoader
 from langchain_text_splitters import TokenTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+# Token Based Splitting
 data = PyPDFLoader("Document loaders/GRU.pdf")
-
-
 docs = data.load()
-
 splitter = TokenTextSplitter(
     chunk_size = 1000,
     chunk_overlap = 10
 )
-
 chunks = splitter.split_documents(docs)
-
 print(chunks[0].page_content)
+
+# Semantic/Meaning Based Text Splitting
--- main.py
+++ main.py
@@ -3,18 +3,28 @@ from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import TextLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_classic.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
 # data = TextLoader("Document loaders/notes.txt")  # This is when you load your txt file and read that 
-data2 = PyPDFLoader("Document loaders/GRU.pdf")  # This is when you load your pdf file and read that
+data = PyPDFLoader("Document loaders/deeplearning.pdf")  # This is when you load your pdf file and read that
 # docs = data.load()
-docs2 = data2.load()
+docs2 = data.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+chunks = splitter.split_documents(docs2)
+
+
 template = ChatPromptTemplate.from_messages(
     [("system", "You are a AI that summarizes the text"),
     ("human", "{data}")]
 )
 model = ChatMistralAI(model="mistral-small-2506")
-prompt = template.format_messages(data = docs2[0].page_content)
+prompt = template.format_messages(data = docs2)
 result = model.invoke(prompt)
 print(result.content)
