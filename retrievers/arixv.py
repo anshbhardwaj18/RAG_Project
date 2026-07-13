39642daf2685cec5908544355a7bba5a2dd84134
Ansh Bhardwaj <anshb457@gmail.com>
2026-07-14 01:17:48 +0530

    Updated Commit

--- retrievers/arixv.py
+++ retrievers/arixv.py
@@ -1,14 +1,14 @@
 from langchain_community.retrievers import ArxivRetriever
 
-retrievers = ArxivRetriever(
+retriever = ArxivRetriever(
     load_max_docs=2,
     load_all_available_meta=True
 )
 
-docs = retrievers.invoke("Large Language Model")
+docs = retriever.invoke("large languagae model")
 
 for i, doc in enumerate(docs):
-    print(f"\nResult {1+1}")
+    print(f"\nResult {i+1}")
     print("Title : ", doc.metadata.get("Title"))
     print("Authors : ", doc.metadata.get("Authors"))
     print("Summary : ", doc.page_content[:500])
