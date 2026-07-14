d680711ea825e3270aa2b748c55cc2cadd4b1b3b
Ansh Bhardwaj <anshb457@gmail.com>
2026-07-14 23:45:53 +0530

    Updated Commit

--- retrievers/mmr.py
+++ retrievers/mmr.py
@@ -9,3 +9,31 @@ docs = [
     Document(page_content="Neural network use gradient descent for training."),
     Document(page_content="Support Vector Machines are Supervised learning algorithm")
 ]
+
+embedding = HuggingFaceEmbeddings()
+
+vectorstore = Chroma.from_documents(docs, embedding)
+
+similarity_retriever = vectorstore.as_retriever(
+    search_type = "similarity",
+    search_kwargs = {"k":3}
+)
+
+print("\n===== Similarity Search Result =====\n")
+
+similarity_docs = similarity_retriever.invoke("What is gradient descent")
+
+for doc in similarity_docs:
+    print(doc.page_content)
+
+mmr_retriever = vectorstore.as_retriever(
+    search_type = "mmr",
+    search_kwargs = {"k":3}
+)
+
+print("\n===== MMR Result =====\n")
+
+mmr_docs = mmr_retriever.invoke("What is gradient descent")
+
+for doc in mmr_docs:
+    print(doc.page_content)
